21005adef74dea61212b7afdf58f04a9f0ed96fb
Romaine Herrera <pizza_aldwyn@yahoo.com>
2014-10-21 16:36:31 -0400

    Create Project and Inject SQLite

---
+++ ProjectManagementTestOne/Model/DBHandler.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Finisar.SQLite;
+
+namespace ProjectManagementTestOne.Model
+{
+    public class DBHandler
+    {
+        private const string DBNAME = "QuestionBank.db";
+
+        SQLiteConnection connection;
+        SQLiteCommand command;
+        //SQLiteDataReader dataReader;
+
+        public DBHandler()
+        {
+            if (File.Exists(DBNAME))
+            {
+                connection = new SQLiteConnection("Data Source=" + DBNAME + ";Version=3;");
+            }
+            else
+            {
+                connection = new SQLiteConnection("Data Source=" + DBNAME + ";Version=3;New=True;");
+            }
+        }
+    }
+}
